4a499fbe639d6a62f562e1b8dff2e8b271d09e1e
hao <xuhaonju@gmail.com>
2020-02-05 13:38:47 +0100

    pakage for HPA-Cell-image-segmeation

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+setuptools.setup(
+    name='pytorch_zoo',
+    author='Hao Xu',
+    version='0.0.0',
+    author_email='hao.xu@scilifelab.se',
+    description='This is copied from DPNUnet repo',
+    url='https://https://github.com/haoxusci/pytorch_zoo',
+    license='GNU',
+    install_requires=[
+    ],
+
+    packages=setuptools.find_packages(),
+    zip_safe=False)
